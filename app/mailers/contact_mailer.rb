07f97397097058a0aa35e5cde0f844b3abf61465
Gustavo T Rudiger <gustavotrudiger@gmail.com>
2017-09-12 20:56:54 +0000

    Fixed contact function name

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
     default to: 'gustavotrudiger@gmail.com'
     
-    def conctact_email(name, email, body)
+    def contact_email(name, email, body)
         @name = name
         @email = email
         @body = body

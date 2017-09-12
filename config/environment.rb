02825dc52d3065bd998b10ef089889df67fb7120
Gustavo T Rudiger <gustavotrudiger@gmail.com>
2017-09-12 20:17:16 +0000

    Added mailer settings to enviroment file

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smt_settings = {
+    :port               => ENV['MAILGUN_SMTP_PORT'],
+    :address            => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name          => ENV['MAILGUN_SMTP_LOGIN'],
+    :password           => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain             => 'frozen-forest-67953.herokuapp.com',
+    :authentication     => :plain,
+}
+ActionMailer:Base.deliver_method = :smtp
